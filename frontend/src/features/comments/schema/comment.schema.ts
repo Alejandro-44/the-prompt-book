0d1ff9de238d6f674ddad5266ef010e14e7a4857
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2025-12-28 21:23:42 -0500

    add error messages in comment schema

--- frontend/src/features/comments/schema/comment.schema.ts
+++ frontend/src/features/comments/schema/comment.schema.ts
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const commentSchema = z.object({
-  content: z.string().min(1)
+  content: z.string("The comment must be a valid string").min(1, "The comment should have at least one character")
 })
 
 export type CommentFormValues = z.infer<typeof commentSchema>
