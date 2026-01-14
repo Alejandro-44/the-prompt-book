0818e46ebe40e1e4b84332679a3b0f186e66f13c
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-14 15:53:21 -0500

    Add sign in and sign up buttons when there is not user

--- frontend/src/components/UserMenu.tsx
+++ frontend/src/components/UserMenu.tsx
@@ -9,11 +9,20 @@ import {
 } from "./ui/menubar";
 import { useAuth, useLogout } from "@/features/auth/hooks";
 import { Avatar, AvatarFallback } from "./ui/avatar";
+import { Button } from "./ui/button";
 
 export function UserMenu() {
-  const { user } = useAuth()
+  const { user } = useAuth();
   const { mutate: logoutUser } = useLogout();
   const navigate = useNavigate();
+
+  const handleSignUp = () => {
+    navigate("/register");
+  };
+
+  const handleSignIn = () => {
+    navigate("/login");
+  };
 
   const handleProfile = () => {
     navigate("/users/me");
@@ -22,6 +31,19 @@ export function UserMenu() {
   const handleLogout = () => {
     logoutUser();
   };
+
+  if (!user) {
+    return (
+      <div className="flex gap-1">
+        <Button onClick={handleSignIn} className="bg-inherit cursor-pointer">
+          Sign In
+        </Button>
+        <Button onClick={handleSignUp} className="bg-inherit border-2 cursor-pointer">
+          Sign Up
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <Menubar className="p-0 m-0 size-fit rounded-full shadow-none border-0">
