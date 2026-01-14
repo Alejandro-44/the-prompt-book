b92273e7a63bf990707e65c72aa24177e0d5c310
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-13 21:27:51 -0500

    change backgorund color in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,12 +3,13 @@ import { UserMenu } from "./UserMenu";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between items-center max-w-6xl mx-auto px-4 h-12">
-      <Link to="/">
-        <span className="font-bold text-xl">ThePromptBook</span>
-      </Link>
-      <nav></nav>
-      <UserMenu />
+    <header className="flex justify-center text-white bg-black">
+      <div className="container flex h-16 items-center justify-between gap-4">
+        <Link to="/">
+          <span className="font-bold text-xl">ThePromptBook</span>
+        </Link>
+        <UserMenu />
+      </div>
     </header>
   );
 };
