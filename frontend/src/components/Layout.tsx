f73dc64c305031018acc420ac81349db913bca5f
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-15 11:32:39 -0500

    modify styles in main layout, use flex to center content

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,7 +5,7 @@ export function Layout() {
   return (
     <>
       <Header />
-      <main className="max-w-7xl mx-auto px-4 py-6">
+      <main className="flex justify-center px-4 py-6">
         <Outlet />
       </main>
     </>
