620afc536b88ac606ce509b7577624266367367b
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-17 17:59:08 -0500

    Add link to sing in and sign up

--- frontend/src/features/auth/pages/LoginPage.tsx
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -1,5 +1,12 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { LoginForm } from "../components/LoginForm";
+import { Link } from "react-router";
 
 export function LoginPage() {
   return (
@@ -11,6 +18,11 @@ export function LoginPage() {
         <CardDescription className="px-6">
           <LoginForm />
         </CardDescription>
+        <CardAction className="px-6">
+          <p className="text-center">
+            Don&apos;t have an account? <Link className="underline hover:text-accent-foreground/75" to="/register">Sign up</Link>
+          </p>
+        </CardAction>
       </Card>
     </div>
   );
--- frontend/src/features/auth/pages/RegisterPage.tsx
+++ frontend/src/features/auth/pages/RegisterPage.tsx
@@ -1,5 +1,12 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { RegisterForm } from "../components/RegisterForm";
+import { Link } from "react-router";
 
 export function RegisterPage() {
   return (
@@ -11,6 +18,12 @@ export function RegisterPage() {
         <CardContent className="px-6">
           <RegisterForm />
         </CardContent>
+        <CardAction className="px-6">
+          <p className="text-center">
+            If you have an account already, you can go to{" "}
+            <Link className="underline hover:text-accent-foreground/75"  to="/login">Sign In</Link>
+          </p>
+        </CardAction>
       </Card>
     </div>
   );
