3b659956e906a47f707f99007f556bc5b03e9c35
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-19 10:36:36 -0500

    Add new hook to implement infinite query

--- frontend/src/features/prompts/hooks/index.ts
+++ frontend/src/features/prompts/hooks/index.ts
@@ -4,3 +4,4 @@ export { usePromptForm } from "./usePromptForm";
 export { usePrompts } from "./usePrompts";
 export { usePromptUpdate } from "./usePromptUpdate";
 export { usePromptDelete } from "./usePromptDelete";
+export { useInfinitePrompts } from "./useInfinitePrompts"
