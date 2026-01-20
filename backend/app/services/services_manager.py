48e111720991bdd6cb67039a024ed1b4c721eee8
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-20 15:50:24 -0500

    add like services to service manager

--- backend/app/services/services_manager.py
+++ backend/app/services/services_manager.py
@@ -1,10 +1,9 @@
-from app.repositories.user_repository import UserRepository
-from app.repositories.prompts_repository import PromptsRepository
-from app.repositories.comments_repository import CommentsRepository
+from app.repositories import UserRepository, PromptsRepository, CommentsRepository, LikesRepository
 from .prompts_service import PromptsService
 from .user_service import UserService
 from .auth_service import AuthService
 from .comments_service import CommentsService
+from .likes_service import LikesService
 
 class ServiceManager:
     def __init__(self, db):
@@ -13,11 +12,15 @@ class ServiceManager:
         self.__user_repo = UserRepository(self.__db)
         self.__prompts_repo = PromptsRepository(self.__db)
         self.__comments_repo = CommentsRepository(self.__db)
+        self.__likes_repo = LikesRepository(self.__db)
         # Services
         self.__user_service = UserService(self.__user_repo)
         self.__auth_service = AuthService(self.__user_repo)
         self.__prompts_service = PromptsService(self.__prompts_repo)
         self.__comments_service = CommentsService(self.__comments_repo)
+        self.__likes_service = LikesService(
+            self.__likes_repo, self.__prompts_repo
+        )
 
     @property
     def user(self) -> UserService:
@@ -34,3 +37,8 @@ class ServiceManager:
     @property
     def comments(self) -> CommentsService:
         return self.__comments_service
+    
+    @property
+    def likes(self) -> LikesService:
+        return self.__likes_service
+
