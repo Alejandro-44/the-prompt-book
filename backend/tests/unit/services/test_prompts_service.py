e0847682cb41dcd6d86ef278c53e5d212adbfeeb
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-21 11:26:52 -0500

    update tests based on new implementation with like by me

--- backend/tests/unit/services/test_prompts_service.py
+++ backend/tests/unit/services/test_prompts_service.py
@@ -18,8 +18,18 @@ MOCK_RANDOM_ID = "507f1f77bcf86cd799439013"
 
 
 @pytest.fixture
-def service(service_factory) -> PromptsService:
-    return service_factory(PromptsService)
+def likes_repo(mocker):
+    return mocker.AsyncMock()
+
+
+@pytest.fixture
+def prompts_repo(mocker):
+    return mocker.AsyncMock()
+
+
+@pytest.fixture
+def service(likes_repo, prompts_repo) -> PromptsService:
+    return PromptsService(prompts_repo, likes_repo)
 
 
 @pytest.fixture
@@ -40,8 +50,8 @@ def mock_prompt():
     }
 
 
-async def test_get_summary_returns_metadata_query(service, mock_repo):
-    mock_repo.get_summary.return_value = ([{
+async def test_get_summary_returns_metadata_query(service, prompts_repo):
+    prompts_repo.get_summary.return_value = ([{
         "_id": ObjectId(),
         "title": "Test Prompt",
         "description": "Test description",
@@ -67,7 +77,7 @@ async def test_get_summary_returns_metadata_query(service, mock_repo):
     }], 2 )
 
     result = await service.get_summary(filters={}, page=2, limit=2)
-    mock_repo.get_summary.assert_awaited_once_with({}, 2, 2)
+    prompts_repo.get_summary.assert_awaited_once_with({}, 2, 2)
 
     prompts = result["items"]
 
@@ -81,22 +91,32 @@ async def test_get_summary_returns_metadata_query(service, mock_repo):
         PromptSummary.model_validate(prompt)
 
 
-async def test_get_one(service, mock_repo, mock_prompt):
-    mock_repo.get_one.return_value = mock_prompt
+async def test_get_one(service, prompts_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
 
     result = await service.get_one(ObjectId(MOCK_PROMPT_ID))
-    mock_repo.get_one.assert_awaited_once_with(ObjectId(MOCK_PROMPT_ID))
+    prompts_repo.get_one.assert_awaited_once_with(ObjectId(MOCK_PROMPT_ID))
     Prompt.model_validate(result)
 
 
-async def test_get_by_id_empty_response_raise_not_found(service, mock_repo):
-    mock_repo.get_one.return_value = {}
+async def test_get_by_id_empty_response_raise_not_found(service, prompts_repo):
+    prompts_repo.get_one.return_value = {}
 
     with pytest.raises(PromptNotFoundError):
         await service.get_one(ObjectId(MOCK_PROMPT_ID))
 
 
-async def test_create_adds_user_id_pub_date_and_author_data(service, mock_repo):
+async def test_get_one_with_like_by_me(service, prompts_repo, likes_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
+    likes_repo.get_prompt_ids_by_user.return_value = [ObjectId(MOCK_PROMPT_ID)]
+    user = User(id=MOCK_USER_ID, handle="user", username="user", is_active=True)
+
+    result = await service.get_one(ObjectId(MOCK_PROMPT_ID), user)
+    Prompt.model_validate(result)
+    assert result.like_by_me == True
+
+
+async def test_create_adds_user_id_pub_date_and_author_data(service, prompts_repo):
     prompt_in = PromptCreate(
         title="Title",
         description="This is a test #ai #nlp",
@@ -112,14 +132,14 @@ async def test_create_adds_user_id_pub_date_and_author_data(service, mock_repo):
         is_active=True
     )
 
-    mock_repo.create.return_value = "some-id"
+    prompts_repo.create.return_value = "some-id"
 
     result = await service.create(user, prompt_in)
 
     assert result == "some-id"
-    mock_repo.create.assert_awaited_once()
+    prompts_repo.create.assert_awaited_once()
 
-    (payload,), _ = mock_repo.create.call_args
+    (payload,), _ = prompts_repo.create.call_args
 
     assert payload["title"] == "Title"
     assert payload["model"] == "gpt-4"
@@ -130,7 +150,7 @@ async def test_create_adds_user_id_pub_date_and_author_data(service, mock_repo):
     assert payload["author_handle"] == user.handle
 
 
-async def test_create_prompt_database_error(service, mock_repo):
+async def test_create_prompt_database_error(service, prompts_repo):
     prompt_in = PromptCreate(
         title="Title",
         description="This is a test prompt #test",
@@ -147,67 +167,67 @@ async def test_create_prompt_database_error(service, mock_repo):
     )
 
 
-    mock_repo.create.side_effect = Exception("DB failure")
+    prompts_repo.create.side_effect = Exception("DB failure")
 
     with pytest.raises(DatabaseError):
         await service.create(user, prompt_in)
 
 
-async def test_update_prompt_success(service, mock_repo, mock_prompt):
-    mock_repo.get_one.return_value = mock_prompt
-    mock_repo.update.return_value = True
+async def test_update_prompt_success(service, prompts_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
+    prompts_repo.update.return_value = True
 
     update_data = PromptUpdate(title="New Title")
     result = await service.update(ObjectId(MOCK_PROMPT_ID), ObjectId(MOCK_USER_ID), update_data)
 
-    mock_repo.get_one.assert_awaited_once_with(ObjectId(MOCK_PROMPT_ID))
-    mock_repo.update.assert_awaited_once()
+    prompts_repo.get_one.assert_awaited_once_with(ObjectId(MOCK_PROMPT_ID))
+    prompts_repo.update.assert_awaited_once()
     assert result is True
 
 
-async def test_update_prompt_with_not_owner_user_unauthorized(service, mock_repo, mock_prompt):
-    mock_repo.get_one.return_value = mock_prompt
+async def test_update_prompt_with_not_owner_user_unauthorized(service, prompts_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
 
     update_data = PromptUpdate(title="New Title")
     with pytest.raises(PromptOwnershipError):
         await service.update(ObjectId(MOCK_PROMPT_ID), ObjectId(MOCK_RANDOM_ID), update_data)
 
 
-async def test_update_prompt_not_found(service, mock_repo):
-    mock_repo.get_one.side_effect = PromptNotFoundError()
+async def test_update_prompt_not_found(service, prompts_repo):
+    prompts_repo.get_one.side_effect = PromptNotFoundError()
     update_data = PromptUpdate(title="New Title")
 
     with pytest.raises(PromptNotFoundError):
         await service.update(ObjectId(MOCK_RANDOM_ID), ObjectId(MOCK_USER_ID), update_data)
 
 
-async def test_update_prompt_raise_error_on_data_layer(service, mock_repo, mock_prompt):
-    mock_repo.get_one.return_value = mock_prompt
-    mock_repo.update.side_effect = Exception()
+async def test_update_prompt_raise_error_on_data_layer(service, prompts_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
+    prompts_repo.update.side_effect = Exception()
 
     update_data = PromptUpdate(title="Same Title")
     with pytest.raises(DatabaseError):
         await service.update(ObjectId(MOCK_PROMPT_ID), ObjectId(MOCK_USER_ID), update_data)
 
 
-async def test_delete_prompt_success(service, mock_repo, mock_prompt):
-    mock_repo.get_one.return_value = mock_prompt
-    mock_repo.delete.return_value = True
+async def test_delete_prompt_success(service, prompts_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
+    prompts_repo.delete.return_value = True
 
     result = await service.delete(ObjectId(MOCK_PROMPT_ID), ObjectId(MOCK_USER_ID))
 
     assert result is True
 
 
-async def test_delete_prompt_not_found(service, mock_repo):
-    mock_repo.get_one.side_effect = PromptNotFoundError()
+async def test_delete_prompt_not_found(service, prompts_repo):
+    prompts_repo.get_one.side_effect = PromptNotFoundError()
 
     with pytest.raises(PromptNotFoundError):
         await service.delete(ObjectId(MOCK_RANDOM_ID), ObjectId(MOCK_USER_ID))
 
 
-async def test_delete_prompt_with_not_owner_user_unauthorized(service, mock_repo, mock_prompt):
-    mock_repo.get_one.return_value = mock_prompt
+async def test_delete_prompt_with_not_owner_user_unauthorized(service, prompts_repo, mock_prompt):
+    prompts_repo.get_one.return_value = mock_prompt
 
     update_data = PromptUpdate(title="New Title")
     with pytest.raises(PromptOwnershipError):
