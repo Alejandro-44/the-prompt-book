4ef5145ec61f3e05107ae10a36cda212954436e8
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2026-01-21 23:20:49 -0500

    add search form

---
+++ frontend/src/features/explore/components/SearchForm.tsx
@@ -0,0 +1,28 @@
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+
+type SearchFormProps = {
+  onSearch: (value: string) => void;
+};
+
+export default function SearchForm({ onSearch }: SearchFormProps) {
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const searchValue = formData.get("search") as string;
+        onSearch(searchValue);
+      }}
+      className="relative group w-full"
+    >
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+      <Input
+        name="search"
+        type="search"
+        placeholder="Search prompts..."
+        className="w-full pl-10"
+      />
+    </form>
+  );
+}
